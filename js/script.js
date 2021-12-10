23d2eee3950cf2efc82fa27a3f9358ad809fd3eb
Zanco1993 <zanconatofabio@gmail.com>
2021-12-10 15:24:33 +0100

    esercizio concluso + commenti e animazioni

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+//dichiaro la variabile contenitore
 const containerBox = document.querySelector(".container-box");
 
+// eseguo il comando partendo da 1 a 100 nella console
 for (let i = 1; i <= 100; i++){
     console.log(i);
 }
@@ -8,16 +10,26 @@ for (let i = 1; i <= 100; i++){
 //     containerBox.innerHTML += `<div class="box">${i}</div>`;
 // }
 
+
+// contatore che parte da 1 a 100 con incremento di 1
+// quando avrà concluso il giro, esci dal ciclo
 for (let i = 1; i <= 100; i++){
+    // se è divisibile sia per 5 che per 3 allora scrivi
+    // FizzBuss
     if (i % 5 === 0 && i % 3 === 0) {
-        // se è divisibile sia per 5 che per 3 allora scrivi
-        //FizzBuss
-        containerBox.innerHTML += `<div class="box">FizzBuss</div>`;
+        containerBox.innerHTML += `<div class="box box-red text-white">FizzBuss</div>`;
+        
+    // se divisibile per 3 allora scrivi Fizz
     } else if (i % 3 === 0) {
-        containerBox.innerHTML += `<div class="box">Fizz</div>`;
+        containerBox.innerHTML += `<div class="box box-green text-bold">Fizz</div>`;
+
+    // se divisibile per 5 allora scrivi Buzz
     } else if (i % 5 === 0){
-        containerBox.innerHTML += `<div class="box">Buzz</div>`;
+        containerBox.innerHTML += `<div class="box box-yellow text-underline">Buzz</div>`;
+
+    // altrimenti se le altre condizioni non sono verificate scrivi 'i'
     } else {
         containerBox.innerHTML += `<div class="box">${i}</div>`;
     }
 }
+        
